050201ca5c425ae2f25509b8c9274ae82b0acf09
pritam kumar singh <94359584+pritamkumar1121@users.noreply.github.com>
2022-03-21 15:37:21 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Ans{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    int[] z = new int[20];
+    int pos = 0;
+    int neg = 0;
+    int odd = 0;
+    int even = 0;
+    int zero = 0;
+    for(int i = 0;i<z.length;i++){
+      System.out.println("Print the value of z["+i+"]");
+      z[i] = s.nextInt();
+
+      if(z[i]>0){
+        pos++;
+      }
+      else if(z[i]<0){
+        neg++;
+      }
+      else{
+        zero++;
+      }
+      if(z[i]%2==0){
+        even++;
+      }
+      else{
+        odd++;
+      }
+    }
+    System.out.println("Positive : "+pos+"\nNegative : "+neg+"\nZero : "+zero+"\nodd : "+odd+"\neven : "+even);
+  }
+}
